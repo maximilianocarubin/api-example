b559f0318ba292c7705f34e2768524702737ec6e
Maximiliano Carubin <mcarubin@folcode.com>
2023-11-07 19:47:24 -0300

    commit

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,7 +3,7 @@ import logger from "../utils/logger";
 
 export const configure = async () => {
   try {
-    await mongoose.connect("mongodb+srv://root:root@cluster0.zy2gvyu.mongodb.net/?retryWrites=true&w=majority");
+    await mongoose.connect("mongodb+srv://root:root@cluster0.zfuk6q2.mongodb.net/?retryWrites=true&w=majority");
     logger.info("🟢 Connected to the database");
   } catch (error) {
     logger.error("🔴 Error connecting to the database:", error);
